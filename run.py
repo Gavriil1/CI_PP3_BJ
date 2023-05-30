dc6ca37ac2376cccd3f89441e3b332fbc1cd18dd
Gavriil1 G <airbnbguide@gmail.com>
2023-05-30 16:36:26 +0000

    Started working on players game, when he make desicions, to take additional card or not

--- run.py
+++ run.py
@@ -12,11 +12,14 @@ def newgame():
     global unlimitted_deck
     unlimitted_deck = [2,3,4,5,6,7,8,10,10,10,10,11]
     print(f" function 0: unlimted deck is {unlimitted_deck}")
+    global player_cards
     player_cards = [] 
     global casino_cards
     casino_cards = []
     print(f"function 0. player_cards array {player_cards} and casino_cards {casino_cards}")
+    global player_points
     player_points = 0
+    global casino_points
     casino_points = 0
     print(f"function 0: player_pints {player_points} and casino points {casino_points}")
 
@@ -28,25 +31,44 @@ print("------------------------------------------------------------------------"
 # Giving two cards two casiono and hiding 1
 def casino_gets_two_cards():
     # Giving first two cards to  casino
-    casino_card_one = casino_cards.append(random.choice(unlimitted_deck))
-    casino_card_two = casino_cards.append(random.choice(unlimitted_deck))
-
-    print(f" casino array is {casino_cards}")
+    casino_cards.append(random.choice(unlimitted_deck))
+    casino_cards.append(random.choice(unlimitted_deck))
+    #casino_cards = [11, 10]
+    print(f"casino array is {casino_cards}")
     print(f"1st card of casino is hidden. Second card is: {casino_cards[1]}")
+    return casino_cards
+
 
 casino_gets_two_cards()
 
-
+print("------------------------------------------------------------------------")
 # Page 2
 # Giving two cards two player.
 def player_gets_two_cards():
     # Giving first two cards to  casino
-    player_card_one = player_cards.append(random.choice(unlimitted_deck))
-    player_card_two = player_cards.append(random.choice(unlimitted_deck))
-    print(player_card_one)
-    print(player_card_two)")
+    #player_cards.append(random.choice(unlimitted_deck))
+    #player_cards.append(random.choice(unlimitted_deck))
+    player_cards = [11,10]
+    print(f"player array {player_cards}")
+    print(f"sum of the array is {sum(player_cards)}")
+    return player_cards
+    
 
-player_gets_two_cards():
+player_gets_two_cards(player_cards)
+print("------------------------------------------------------------------------")
+
+print("player starts it game in the function bellow")
+
+def player_start_first_playing(player_cards, player_points):
+    print(f"sum of the array is {sum(player_cards)}")
+    if sum(player_cards) == 21:
+        print("The player won this round")
+        player_points = player_points + 1
+        print(f"The score is player: {player_points} casino: {casino_points }")
+        return player_points
+
+
+player_start_first_playing(player_cards, player_points)
 
 '''
 # Giving first two cards to player 
