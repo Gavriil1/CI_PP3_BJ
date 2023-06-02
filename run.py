7c17df1a2c9a9d8c98813104fb453379c3519e11
Gavriil1 G <airbnbguide@gmail.com>
2023-06-02 16:00:37 +0000

    changed text of the game

--- run.py
+++ run.py
@@ -43,49 +43,58 @@ def casino_start_first_playing():
     global casino_cards, casino_points, player_points
     if sum(casino_cards) == 22:
         casino_cards[1] = 1
-        print("Since casino have to aces and sum of them is 22, the second Ace counts as 1")
+        print("1 Casino has to Aces. First counts 11 second 1")
     if sum(casino_cards) == 21:
         print("CASINO WINS! BLACK JACK!")
         casino_points = casino_points + 1
-        print(f"Score Player: {player_points} Casino: {casino_points}")
-        return casino_points
-    
-    print(f"The sum of casino cards is {sum(casino_cards)} and of player cards {sum(player_cards)}")
+        print(f"2 SCORE PLAYER: {player_points} CASINO: {casino_points}")
+        return 
+        
+    print(f"3 The sum of casino cards is {sum(casino_cards)} and of player cards {sum(player_cards)}")
 
     if sum(casino_cards) > sum(player_cards):
-        print(f"cards sum of the casino: {sum(casino_cards)} sum of the player cards {sum(player_cards)}")
-        print("Casiono SUmo of cards is higher that of a player. Casion won")
-        print("Casino won this round")
+        print(f"4 CASINO SUM: {sum(casino_cards)} PLAYER SUM {sum(player_cards)}")
+        print("5 CASINO SUM > PLAYER SUM. CASINO WINS!")
         casino_points = casino_points + 1
-        print(f"The score is player: {player_points} casino: {casino_points}")
+        print(f"6 SCORE PLAYER: {player_points} CASINO: {casino_points}")
+        return
+  
     while sum(casino_cards) < 17:
-        print("I am taking new card")
+        print("7 CASINO SUM < 17. CASINO TAKE A  NEW CARD!")
         casino_cards.append(random.choice(unlimitted_deck))
         print(casino_cards)
         if sum(casino_cards) >21:
-            print("Casino sum is over 21. Casino lost player won")
+            print("8 CASINO SUM > 21. PLAYER WINS!")
             player_points = player_points + 1
-            print(f"The score is player: {player_points} casino: {casino_points}")
+            print(f"9 SCORE PLAYER: {player_points} CASINO: {casino_points}")
+            return
         if sum(casino_cards) == 21:
-            print("Casino sum is  21. Casino won player lost")
+            print("10 CASINO BLACK JACK ! CASINO WINS!")
             casino_points = casino_points + 1
-            print(f"The score is player: {player_points} casino: {casino_points}")
+            print(f"11 SCORE PLAYER: {player_points} CASINO: {casino_points}")
+            return
         if sum(casino_cards) > sum(player_cards):
-            print("Casino sum is  iver player sum. Casino won player lost")
+            print("12 CASINO SUM > PLAYER SUM. CASINO WINS!")
             casino_points = casino_points + 1
-            print(f"The score is player: {player_points} casino: {casino_points}")
+            print(f"13 SCORE PLAYER: {player_points} CASINO: {casino_points}")
+            return
  
     if sum(casino_cards) > sum(player_cards):
-        print("Casino sum is  iver player sum. Casino won player lost")
+        print("14 CASINO SUM > PLAYER SUM. CASINO WINS!")
         casino_points = casino_points + 1
-        print(f"The score is player: {player_points} casino: {casino_points}")
+        print(f"SCORE PLAYER: {player_points} CASINO: {casino_points}")
+        return
+
     if sum(casino_cards) < sum(player_cards):
-        print("Casino sum is  lower than  player sum. Player won player lost")
+        print("15 CASINO SUM < PLAYER SUM. PLAYER WINS!")
         player_points = player_points + 1
-        print(f"The score is player: {player_points} casino: {casino_points}")
+        print(f"SCORE PLAYER: {player_points} CASINO: {casino_points}")
+        return
+
     if sum(casino_cards) == sum(player_cards):
-        print("Draw")
-        print(f"The score is player: {player_points} casino: {casino_points}")
+        print("16 CASINO SUM = PLAYER SUM. DRAW!")
+        print(f"17 SCORE PLAYER: {player_points} CASINO: {casino_points}")
+        return
 
 
 def player_start_first_playing():
@@ -94,31 +103,32 @@ def player_start_first_playing():
     '''
     global player_cards, player_points, casino_points
     if sum(player_cards) == 21:
-        print("The player won this round")
+        print("PLAYER SUM 21. BLACK JACK! PLAYER WINDS!")
         player_points = player_points + 1
-        print(f"The score is player: {player_points} casino: {casino_points}")
+        print(f"SCORE PLAYER: {player_points} CASINO: {casino_points}")
         return player_points
     if sum(player_cards) == 22:
         player_cards[1] = 1
-        print("Since you have to aces and sum of them is 22, the second Ace counts as 1")
-    print(f"Player, you have {len(player_cards)} cards. The cards have the following values")
+        print("PLAYER HAS TWO ACES. SECOND COUNTS AS  1")
+    print(f"SCORE PLAYER: {player_points} CASINO: {casino_points}")
+    print(f"YOU HAVE {len(player_cards)} cards. The cards VALUES ARE:")
     for card in player_cards:
         print(f"    {card}")
-    print(f"The sum of the cards is {sum(player_cards)}")
-    print(f"casino first card is {casino_cards[0]}")
-    print(f" Do you want additional card? Please press '1' for yes and '0' for No")
+    print(f"PLAYER SUM {sum(player_cards)}")
+    print(f"CASINO FIRST CARD {casino_cards[0]}")
+    print(" DO YOU WANT ADDITIONAL CARD? PRESS '1' FOR YES, '0' FOR No")
 
     
     valid_entry = False
 
     while not valid_entry:
         user_response_str = input("Enter a number: ")
-        print(user_response_str)
-        print(type(user_response_str))
+        # print(user_response_str)
+        # print(type(user_response_str))
         if user_response_str.isnumeric():
             user_response = int(user_response_str)
-            print(type(user_response))
-            print(user_response)
+            # print(type(user_response))
+            # print(user_response)
             if user_response == 0 or user_response == 1:
                  valid_entry = True
             else:
@@ -171,6 +181,7 @@ def player_start_first_playing():
         casino_start_first_playing()
 
 
+
 def main():
     global player_points
     player_points = 0
@@ -189,3 +200,8 @@ def main():
         print("adddddddddddddddddddddddddddd")
         k = k+1
         
+
+
+main()
+
+
